bd9f5d3f7be16aa007a12b2fbba23a1db7f38714
Lourdes Jeunette Manzo Ramírez <jeunettemanzo@gmail.com>
2019-05-17 10:26:50 -0500

    add directory components

---
+++ my-app/src/components/BtnAdd.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const BtnAdd = () => {
+    return <h1>Holi</h1>
+}
+
+
+export default BtnAdd
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BtnAdd from './components/BtnAdd';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BtnAdd />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
