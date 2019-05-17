48cd1f215216b418d9170b00cec20025eaef8142
Lourdes Jeunette Manzo Ramírez <jeunettemanzo@gmail.com>
2019-05-17 10:31:11 -0500

    add directory screenTable

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BtnAdd from './components/BtnAdd';
+import BtnAdd from './components/screenTable/BtnAdd';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<BtnAdd />, document.getElementById('root'));
