a39885d6248e74c79dc20df0f1ab97efa8bc300d
Lourdes Jeunette Manzo Ramírez <jeunettemanzo@gmail.com>
2019-05-21 17:50:10 -0500

    add component CommandContainer

---
+++ my-app/src/components/containers/CommandContainer.js
@@ -0,0 +1,35 @@
+// import React from 'react';
+// import NavBarTables from '../NavBarTables';
+// import Button from '../Button';
+// import employees from '../data/employees'
+//
+//
+// // const employeesBar = employees.bar.map( employee => employee);
+// // //
+// // //
+// // console.log(employeesBar);
+//
+// class CommandContainer extends React.Component {
+//
+//   employeesBar = employees.bar.map( employee => employee);
+//
+//   render () {
+//     return (
+//       <section className="command-container">
+//         <NavBarTables/>
+//         {
+//           this.employeesBar.map((btn) => {
+//             return (
+//               <Button className="btn-ready" label={btn}/>
+//             )
+//           })
+//         }
+//       </section>
+//     )
+//
+//
+//   }
+//
+// }
+//
+// export default CommandContainer
