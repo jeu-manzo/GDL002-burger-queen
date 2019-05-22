a46cbef269f328385923e41cfb451642d8782610
Lourdes Jeunette Manzo Ramírez <jeunettemanzo@gmail.com>
2019-05-22 15:45:32 -0500

    add data to file food

--- my-app/src/data/food.js
+++ my-app/src/data/food.js
@@ -1,6 +1,310 @@
 let food = {
 
+  "breakfast": {
+    "food": {
+      "Chilaquiles": [
+        {
+          name: "Verdes",
+          price: 45,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Rojos",
+          price: 45,
+          stock: true,
+          order: null
+        }
+      ],
+      "Huevos": [
+        {
+          name: "Rancheros",
+          price: 45,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Revuletos (Jamón, Chorizo o Tocino)",
+          price: 45,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Estrellados",
+          price: 45,
+          stock: true,
+          order: null,
+        },
+        {
+          name: "Mexicana",
+          price: 45,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Ingredientes",
+          price: 45,
+          stock: true,
+          order: null
+        }
+      ],
+      "Quesadillas": [
+        {
+          name: "Jamon",
+          price: 40,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Tocino",
+          price: 45,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Champiñones",
+          price: 45,
+          stock: true,
+          order: null
+        }
+      ],
+      "Molletes": [
+        {
+          name: "Salados",
+          price: 40,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Dulces",
+          price: 35,
+          stock: true,
+          order: null
+        }
+      ]
 
+    },
+    "drinks": {
+      "Café": [
+        {
+          name: "Espresso Americano",
+          price: 25,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Espresso",
+          price: 25,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Cappuccino",
+          price: 30,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Cappuccino Moka",
+          price: 35,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Latte",
+          price: 30,
+          stock: true,
+          order: null,
+        },
+        {
+          name: "Late Moka",
+          price: 35,
+          stock: true,
+          order: null
+        }
+      ],
+      "Jugo": [
+        {
+          name: "Naranja",
+          price: 30,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Zanahoria",
+          price: 30,
+          stock: true,
+          order: null
+        }
+      ],
+      "Bebidas": [
+        {
+          name: "Chocolate",
+          price: 35,
+          stock: true,
+          order: null
+        },
+        {
+          name: "ChocoMilk",
+          price: 35,
+          stock: true,
+          order: null
+        }
+      ]
+    }
+  },
+  "lunchDinner": {
+    "food": {
+      "Paninis": [
+        {
+          name: "Hawaiano",
+          price: 55,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Italiano",
+          price: 60,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Pavo",
+          price: 60,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Pollo",
+          price: 60,
+          stock: true,
+          order: null
+        }
+      ],
+      "Sandwiches": [
+        {
+          name: "Hawaiano",
+          price: 50,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Italiano",
+          price: 55,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Jamón",
+          price: 45,
+          stock: true,
+          order: null,
+        },
+        {
+          name: "Pavo",
+          price: 45,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Pollo",
+          price: 45,
+          stock: true,
+          order: null
+        }
+      ],
+      "Pizza": [
+        {
+          name: "1 Ingrediente",
+          price: 89,
+          stock: true,
+          order: null
+        },
+        {
+          name: "2 Ingredientes",
+          price: 99,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Hawaiana",
+          price: 99,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Especial",
+          price: 120,
+          stock: true,
+          order: null
+        }
+      ],
+      "Wraps": [
+        {
+          name: "Vegetariano",
+          price: 55,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Arrachera",
+          price: 60,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Pollo",
+          price: 55,
+          stock: true,
+          order: null
+        }
+      ]
+    },
+    "drinks": {
+      "Licores": [
+        {
+          name: "Copa de Vino Tinto",
+          price: 60,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Sangría",
+          price: 65,
+          stock: true,
+          order: null
+        }
+      ],
+      "Cervezas": [
+        {
+          name: "Corona Light",
+          price: 35,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Corona",
+          price: 35,
+          stock: true,
+          order: null
+        }
+      ],
+      "Bebidas": [
+        {
+          name: "Rusa",
+          price: 45,
+          stock: true,
+          order: null
+        },
+        {
+          name: "Limonada",
+          price: 35,
+          stock: true,
+          order: null
+        }
+      ]
+    }
+  }
 
 };
 
