e3b5fa529e394922d42853eceddc2cb09b4e087f
Lourdes Jeunette Manzo Ramírez <jeunettemanzo@gmail.com>
2019-05-28 10:35:50 -0500

    Updates

---
+++ precache-manifest.3c577fe7c6b68c1111f665c683cb8fa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed262f453c03940eab4c702cd7a0530",
+    "url": "/GDL002-burger-queen/inicio/index.html"
+  },
+  {
+    "revision": "70af336334db6b1b09aa",
+    "url": "/GDL002-burger-queen/inicio/static/css/main.3ae677d9.chunk.css"
+  },
+  {
+    "revision": "f319353af4ae236f91ec",
+    "url": "/GDL002-burger-queen/inicio/static/js/2.2aee02a8.chunk.js"
+  },
+  {
+    "revision": "70af336334db6b1b09aa",
+    "url": "/GDL002-burger-queen/inicio/static/js/main.fa936611.chunk.js"
+  },
+  {
+    "revision": "cdd48d09d1dee9d5fe08",
+    "url": "/GDL002-burger-queen/inicio/static/js/runtime~main.b3fd3958.js"
+  }
+]);
