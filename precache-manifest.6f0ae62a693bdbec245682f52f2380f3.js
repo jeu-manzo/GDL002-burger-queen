17a7bc57d83da1f69369961de019d97f1d2abda0
Jeu Manzo <jeunettemanzo@gmail.com>
2019-06-05 09:03:47 -0500

    Updates

---
+++ precache-manifest.6f0ae62a693bdbec245682f52f2380f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccc3e75c8ba2fdc1c4f1ba1921a5c44",
+    "url": "/GDL002-burger-queen/my-app/index.html"
+  },
+  {
+    "revision": "ee943112c55c0b4ec74b",
+    "url": "/GDL002-burger-queen/my-app/static/css/main.fcbf991b.chunk.css"
+  },
+  {
+    "revision": "7575e0017091c41b1350",
+    "url": "/GDL002-burger-queen/my-app/static/js/2.494c0359.chunk.js"
+  },
+  {
+    "revision": "ee943112c55c0b4ec74b",
+    "url": "/GDL002-burger-queen/my-app/static/js/main.2880c332.chunk.js"
+  },
+  {
+    "revision": "d0723b78e3a3ee3e8c86",
+    "url": "/GDL002-burger-queen/my-app/static/js/runtime~main.bbf35289.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GDL002-burger-queen/my-app/precache-manifest.45bace250781af7f9a93f2bf2495e2de.js"
+  "/GDL002-burger-queen/my-app/precache-manifest.6f0ae62a693bdbec245682f52f2380f3.js"
 );
 
 self.addEventListener('message', (event) => {
